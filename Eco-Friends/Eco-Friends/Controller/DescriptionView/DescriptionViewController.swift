f1478bc7bbcf2c4864166b5d8ed4df782fa12492
ByoungilYoun <jimmy7525@naver.com>
2020-07-16 15:51:56 +0900

    View - DividerView - ButtonView - ScrollView
    Controller
    - HomeViewController
    - OrderViewController
    - SearchViewController
    - DescriptionViewController

---
+++ Eco-Friends/Eco-Friends/Controller/DescriptionView/DescriptionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DescriptionViewController.swift
+//  Eco-Friends
+//
+//  Created by 윤병일 on 2020/07/15.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .brown
+  }
+}
